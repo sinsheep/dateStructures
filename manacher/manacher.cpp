86d73935faf085de90f3856687f0c20e27e1a061
ysx <691765069@qq.com>
2020-12-09 19:56:57 +0800

    compelete manacher

--- manacher/manacher.cpp
+++ manacher/manacher.cpp
@@ -1,6 +1,8 @@
 #include<stdio.h>
+#define max(a,b) (a)>(b)? (a):(b)
+#define min(a,b) (a)>(b)? (b):(a)
 #define MAXL 3000005
-int len[MAXL];
+int len[MAXL],l,m=0;
 char str[MAXL],s[MAXL];
 void init(){
   int k=0;
@@ -9,7 +11,26 @@ void init(){
     str[k++]='#';
     str[k++]=s[i];
   }
-  str[k]='%';
+  str[k]='#';
+  str[k+1]='%';
+  l=k;
+}
+void manacher(){
+    int mx=0,id;
+    for(int i=0;i<l;i++){
+        if(i<mx){
+            len[i]=min(len[2*id-i],mx-i);
+        }else{
+            len[i]=1;
+        }
+        while(str[i+len[i]]==str[i-len[i]])
+            len[i]++;
+        if(i+len[i]>mx){
+            mx=i+len[i];
+            id=i;
+        }
+        m=max(m,len[i]-1);
+    }
 }
 int main()
 {
@@ -18,5 +39,9 @@ int main()
   while(n--)
   {
     scanf("%s",s);
+    init();
+    m=0;
+    manacher();
+    printf("%d\n",m);
   }
 }
