bd1b915a24f279398da04e19591309f64c9c3149
ysx <691765069@qq.com>
2020-12-25 20:53:53 +0800

    adjust findKthNum

--- divideAndConquer/FindKthNum.cpp
+++ divideAndConquer/FindKthNum.cpp
@@ -7,7 +7,7 @@ int randomized_partition(int A[],int low,int high){
     int i=low-1;
     int j=rand()%(high-low+1)+low;
     swap(A[high],A[j]);
-    for(int j=low;j<high;++j){
+    for(j=low;j<high;++j){
         if(A[j]<=A[high]){
             swap(A[++i],A[j]);
         }
@@ -15,20 +15,33 @@ int randomized_partition(int A[],int low,int high){
     swap(A[high],A[++i]);
     return i;
 }
+int randomized_partition1(int A[],int low,int high){
+    int i=low;
+    int x=rand()%(high-low)+low;
+    swap(A[low],A[x]);
+    for(int j=low+1;j<high;j++){
+        if(A[j]<=A[low]){
+            swap(A[++i],A[j]);
+        }
+    }
+    swap(A[low],A[i]);
+    return i;
+}
 int randomized_select(int A[],int low,int high,int i){
     if(low==high)return A[low];
-    int q=randomized_partition(A,low,high);
+    int q=randomized_partition1(A,low,high);
     int k=q-low+1;
     if(k==i)return A[q];
     else if(i<k)
-        return randomized_select(A,low,q-1,i);
+        return randomized_select(A,low,q,i);
     else
         return randomized_select(A, q+1, high, i-k);
 }
 int main(){
     srand(time(0));
-    int a[9] = {3, 5, 7, 9, 8, 6, 1, 2, 4};
-    printf("\n%d\n",randomized_select(a, 0, 9, 4));
+    int a[10] = {3, 5, 7, 9, 8, 6, 1, 2, 4,10};
+    printf("%d\n",randomized_select(a, 0, 9, 9));
+    // printf("%d\n",randomized_select(a, 0, 9, 4));
     for(int i=0;i<9;i++){
         printf("%d ",a[i]);
     }
