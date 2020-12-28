3751cb8eb87c80c9fdb501f8fcb9cbd085012338
ysx <691765069@qq.com>
2020-12-28 22:13:06 +0800

    add randomized quickSort

--- sort/quickSort.cpp
+++ sort/quickSort.cpp
@@ -1,4 +1,7 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<iostream>
 int Partition(int A[],int low,int high){
     int pivot = A[low];
     while(low<high){
@@ -21,36 +24,42 @@ void QuickSort(int A[], int low, int high){
         QuickSort(A,pivot+1,high);
     }
 }
-// void swap(int *a,int *b){ 
-//     int tmp=*a;
-//     *a=*b;
-//     *b=*a;
-// }
 int Partition2(int A[],int p,int q){
     int i=p,j,x=A[p];
     for(j=p+1;j<q;j++){
         if(A[j]<x){
             i++;
-            int tmp=A[j];
-            A[j]=A[i];
-            A[i]=tmp;
+            std::swap(A[j],A[i]);
         }
     }
-    int tmp=A[p];
-    A[p]=A[i];
-    A[i]=tmp;
+    std::swap(A[p],A[i]);
+    return i;
+}
+int PartitionRandomize(int A[],int p,int q){
+
+    int r=rand()%(p-q)+p;
+    std::swap(A[r],A[p]);
+    int i=p,j,x=A[p];
+    for(j=p+1;j<q;j++){
+        if(A[j]<x){
+            i++;
+            std::swap(A[j],A[i]);
+        }
+    }
+    std::swap(A[p],A[i]);
     return i;
 }
 void QuickSort2(int A[],int p,int q){
     if(p<q){
+        // int pivot=PartitionRandomize(A,p,q);
         int pivot=Partition2(A,p,q);
-        printf("pivot=%d\n",pivot);
         QuickSort2(A,p,pivot);
         QuickSort2(A,pivot+1,q);
     }
 }
 int main(){
     // printf("%d %d",a,b);
+    srand(unsigned(time(NULL)));
     int A[]={1,2,6832,32812,894321,892,27,298};// {6,10,13,5,8,3,2,1};
     QuickSort2(A,0,8);
     for(int i=0;i<8;i++){
