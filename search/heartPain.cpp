b004a34dc19d3fb5ccb6210c697a6d7f6eae5f4e
ysx <691765069@qq.com>
2021-02-03 21:04:06 +0800

    add binarySearch

---
+++ search/heartPain.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    ll n, l=1, r;
+    cin >> n;
+    r = n >= 8 ? n: 8;
+    ll ans=0;
+    while(l <= r){
+        ll mid = (l + r) >> 1;
+        ll d = int( mid * log10(mid) ) + 1;
+        if(d >= n){
+            ans=mid;
+            r = mid - 1;
+        }else l = mid + 1;
+    }
+    cout << ans;
+    
+    return 0;
+}
