03e3af27c6b32dea08c07f1480f01078e7084343
ysx <691765069@qq.com>
2021-02-19 22:09:33 +0800

    add c

---
+++ codeforces/div2/Round699/C.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fi first
+#define se second
+#define inf 0x3f3f3f3f;
+#define Fep(i, a, b) for (int i = (int)(a); i <= (int)(b); i++)
+#define Rep(i, a, b) for (int i = (int)(a); i >= (int)(b); i--)
+#define see(x) cout << (x) << '\n'
+typedef double db;
+using namespace std;
+typedef pair<int, int> pii;
+int main(){
+
+    int t, n, m;
+    cin >> t;
+    while (t--) {
+        cin >> m;
+    }
+    return 0;
+}
+
